2b9c161b3bced89d2b2670fd43945b3ac6cfbc9e
Nagesh2003 <codeinovator@gmail.com>
2025-07-25 07:36:42 +0530

    refactor:enhance folder structure for better understable

--- springDeploy/src/main/java/com/nagesh/complaintsystem/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nagesh.complaintsystem.repository;
- 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagesh.complaintsystem.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
